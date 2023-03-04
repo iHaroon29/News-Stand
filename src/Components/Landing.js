367c7d9b937779b03b059d91292f2b1c49b25b60
Faiz <faizmohammedkhan7@gmail.com>
2023-03-04 13:12:42 +0100

    Landing transitioning layout. Image not showing up for some reason. Please check

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,52 +5,75 @@ import Hamber from './Hamber'
 import Onborading from './Onboarding'
 import FooterToggle from './FooterToggle'
 import About from './about'
+import "../App.css";
 
-const Landing = () => {
-  const [visible, setVisible] = useState(false)
-  const [overlay, setOverlay] = useState(false)
+
+const MainPage = () => {
+  const [showAbout, setShowAbout] = useState(false);
   const [animate, setAnimate] = useState(false)
-  const [aboutOverlay, setAboutOverlay] = useState(false)
-  const [aboutVisible, setAboutVisible] = useState(false)
-  const [isActive, setIsActive] = useState(false)
-  const updateFormState = () => {
-    setOverlay((prev) => !prev)
-    setVisible((prev) => !prev)
-    setAnimate((prev) => !prev)
-  }
-  const updateAboutState = () => {
-    setAboutOverlay((prev) => !prev)
-    setAboutVisible((prev) => !prev)
-    setIsActive((prev) => !prev)
-  }
+
+  const handleButtonClick = () => {
+    console.log("Button clicked");
+    setShowAbout(!showAbout);
+    setAnimate(!animate);
+    console.log(showAbout);
+  };
+  
 
   return (
-    <div className='landing-wrapper'>
-      <div
+      <div className="parent-container">
+        <div
         className={!animate ? 'test uncurve' : 'test curve'}
-        onClick={updateFormState}
+        onClick={handleButtonClick}
       >
         <Hamber animate={animate} />
       </div>
-
-      {overlay ? (
-        <OverlayWrapper
-          width='100%'
-          height='100%'
-          saturation='0.15'
-          zIndex='0'
-        />
-      ) : null}
-      <div className='landing-image-holder'></div>
-      <Onborading visible={visible} />
-      <Notif />
-      <div className={!animate} onClick={updateAboutState}>
-        <FooterToggle isActive={isActive} />
+        <Landing visible={showAbout} />
+        <div className={`about-info ${showAbout ? "slide-in" : ""}`}>
+          <div>About info</div>
+        </div>
       </div>
-      {aboutOverlay ? <div className='about-overlay'></div> : null}
-      <About visible={aboutVisible} />
+  );
+}
+
+const Landing =(props) =>{
+  var {visible} = props
+  return(
+    <div className={`landing-page ${visible ? "slide-out" : ""}`}>
+      <div className='landing-page-image'>
+
+      </div>
+      <div className='landing-page-text'>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt tempor eros, 
+            ut consectetur libero dictum eu. Maecenas lacinia gravida dolor vel venenatis. Morbi 
+            volutpat pellentesque nisi quis placerat. Donec rutrum nulla ac lectus iaculis laoreet. 
+            Proin viverra cursus ligula nec tristique. In porta sagittis porttitor. In consequat porta suscipit.
+      </div>
     </div>
   )
 }
+// const Landing = (props) =>{
+//   const {visible} = props
+//   return(
+//     <div className={`landing-page ${visible ? "slide-out" : ""}`}>
+//       <div className='image-block'>
 
-export default Landing
+//       </div>
+//       <div className='text-block'>
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// const About = (props) =>{
+//   const {visible} = props
+//   return (
+//     <div className={`about-info ${visible ? "slide-in" : ""}`}>
+
+//     </div>
+//   )
+// }
+
+export default MainPage;
+
